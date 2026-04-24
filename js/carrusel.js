d4b68376986e22fdcd9853de6a49d20d5280d081
Jefferson Velarde Guevara <jeffersonvelarde14@gmail.com>
2026-04-24 10:15:19 -0500

    Update carrusel.js

--- js/carrusel.js
+++ js/carrusel.js
@@ -5,3 +5,13 @@ function moverCarrusel(direccion) {
     indice = (indice + direccion + totalCards) % totalCards;
     contenedor.style.transform = `translateX(-${indice * 800}px)`;
 }
+
+let indiceENLA = 0;
+
+function moverCarruselENLA(direccion) {
+    const contenedor = document.getElementById('projects-container-enla');
+    const totalCards = contenedor.children.length;
+
+    indiceENLA = (indiceENLA + direccion + totalCards) % totalCards;
+    contenedor.style.transform = `translateX(-${indiceENLA * 800}px)`;
+}
